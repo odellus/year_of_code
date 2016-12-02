3baec5bf89d45a69d35e269956e51d5230d6820b
Thomas Wood <thomas@wgapl.com>
2016-12-02 04:16:20 -0800

    Added tape_equilibrium.py

---
+++ tape_equilibrium.py
@@ -0,0 +1,22 @@
+# Author: Thomas Wood -- thomas@synpon
+
+def solution(A):
+    # write your code in Python 2.7
+    import sys
+    if len(A) == 2:
+        return abs(A[0] - A[1])
+
+    scan = []
+    s = 0
+    for x in A:
+        s += x
+        scan.append(s)
+        
+
+    mindiff = sys.maxint
+    for x in scan[:-1]:
+        diff = abs(s - 2*x)
+        if diff < mindiff:
+            mindiff = diff
+
+    return mindiff
