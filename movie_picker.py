bfa2b30893bcc12f46e73ac34c63b5b05b27af5f
Thomas Wood <odell.wood@gmail.com>
2022-06-08 10:48:18 -0700

    We don't need to be testing inside the timing loop.

--- movie_picker.py
+++ movie_picker.py
@@ -2,23 +2,6 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
-def _movie_picker(flight_length, movie_lengths):
-    """
-    We're give a flight_length and a list of flim lengths.
-    We want to find a pair of movies that equal the flight time.
-    We will return the index of two movies who's combined length
-    is equal to the flight length.
-    """
-    movie_idx = {x: k for (k, x) in enumerate(movie_lengths)}
-    for val, idx in movie_idx.items():
-        comp = flight_length - val
-        if comp in movie_idx:
-            comp_idx = movie_idx[comp]
-            if comp_idx == idx: continue
-            return (idx, comp_idx)
-
-
-
 def movie_picker(flight_length, movie_lengths):
     """
     We're give a flight_length and a list of flim lengths.
@@ -41,7 +24,6 @@ def movie_picker(flight_length, movie_lengths):
 def get_movie_lengths(size):
     return np.random.randint(80, 200, size=(size,)).tolist()
 
-
 def test_picks(flight_length, movie_lengths, picks):
     pair_sums = [movie_lengths[x] + movie_lengths[y] for (x,y) in picks]
     assert len([x for x in pair_sums if x != flight_length]) == 0
@@ -53,9 +35,8 @@ def time_algo(flight_length=240):
         movie_lengths = get_movie_lengths(size)
         t = time.time()
         picks = movie_picker(flight_length, movie_lengths)
-        test_picks(flight_length, movie_lengths, picks)
         times.append(time.time()-t)
-    idxs = [x for x in range(10)]
+        test_picks(flight_length, movie_lengths, picks)
     plt.plot(sizes, times)
     plt.xlabel('len(movie_lengths)')
     plt.ylabel('Runtime (sec)')
